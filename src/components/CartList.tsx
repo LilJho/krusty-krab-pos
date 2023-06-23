e29b3ff89f4f5c8aefcea92e7547803e6ddbdddd
LilJho <jhonnelg25@gmail.com>
2023-06-24 02:40:32 +0800

    added no data ui in cart and added remove item from cart functionality

--- src/components/CartList.tsx
+++ src/components/CartList.tsx
@@ -1,23 +1,44 @@
 import { Card, CardContent } from "../components/ui/card";
-import { useAppSelector } from "../hooks/hooks";
+import { useAppSelector, useAppDispatch } from "../hooks/hooks";
+import { removeFromCart } from "../lib/store/slice/cartSlice";
 import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+import { MdDeleteOutline } from "react-icons/md";
+import NodataSvg from "../assets/nodata.svg";
 
 const CartList = () => {
   const cartItems = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
+
   return (
     <Card>
       <CardContent>
-        {cartItems.map((item) => (
-          <Card className="flex items-center justify-between p-2 m-4">
-            <img
-              className="w-[4rem] h-[4rem] object-contain"
-              src={item.img}
-              alt={item.name}
-            />
-            <p>{item.name}</p>
-            <Badge>Price: 2$</Badge>
+        {cartItems.length === 0 ? (
+          <Card className="mt-4">
+            <CardContent className="flex flex-col items-center justify-center">
+              <img src={NodataSvg} alt="No data Picture" />
+              <p>No Data Found...</p>
+            </CardContent>
           </Card>
-        ))}
+        ) : (
+          cartItems.map((item) => (
+            <Card className="flex items-center justify-between p-2 m-4">
+              <img
+                className="w-[4rem] h-[4rem] object-contain"
+                src={item.img}
+                alt={item.name}
+              />
+              <p>{item.name}</p>
+              <Badge>Price: 2$</Badge>
+              <Button
+                variant={"outline"}
+                onClick={() => dispatch(removeFromCart(item))}
+              >
+                <MdDeleteOutline />
+              </Button>
+            </Card>
+          ))
+        )}
       </CardContent>
     </Card>
   );
